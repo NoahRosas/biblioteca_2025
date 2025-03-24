5e21b7c3d5ea5c45001555497fdfe565a099fbf4
Noah Rosas <nrosas@itpfp.com>
2025-03-24 13:53:51 +0100

    creadas las migraciones de piso a libros

--- resources/js/pages/users/Create.tsx
+++ resources/js/pages/users/Create.tsx
@@ -15,11 +15,12 @@ export default function CreateUser({ permisos, roles}: CreateUserProps) {
   const { t } = useTranslations();
   return (
     <UserLayout title={t('ui.users.create')}>
-            <h3 className="flex-col ml-auto mr-auto mb-2">
+            <h3 className="flex ml-auto mr-auto mt-2 mb-2">
                 <User className="mr-2" />
                 {t('ui.users.create')}
-                <p className="text-s text-muted-foreground">{t('ui.users.extra_info.create_user')}</p>
+                
             </h3>
+            <p className="text-s text-center text-muted-foreground">{t('ui.users.extra_info.create_user')}</p>
             
             <UserForm permisos={permisos} roles={roles} />
         </UserLayout>
--- resources/js/pages/users/Edit.tsx
+++ resources/js/pages/users/Edit.tsx
@@ -27,7 +27,6 @@ export default function EditUser({ user, page, perPage, permisos, roles, userPer
     <h3 className="mr-auto mb-2 ml-auto flex mt-2">
         <User className="mr-2" />
         {t('ui.users.edit')}
-       
     </h3>
     <UserForm permisos={permisos} userPermits={userPermits} roles={roles} initialData={user} page={page} perPage={perPage} />
 </UserLayout>
